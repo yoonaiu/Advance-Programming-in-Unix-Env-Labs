a3757a9b5e25cc9f74b37e21b03795107c8e05f5
yoonaiu <free.cs09@nycu.edu.tw>
2023-05-30 22:59:37 +0800

    init finish, working on breakpoint

--- hw2/sdb.cpp
+++ hw2/sdb.cpp
@@ -1,112 +1,109 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
-
 #include <capstone/capstone.h>
 
-#include "ptools.h"
-
+#include <iostream>
+#include <cstring>
 #include <string>
+#include <sstream>
+#include <vector>
 #include <map>
 
-#include <iostream>
-#include <vector>
-#include <sstream>
-
 using namespace std;
-
-#define	PEEKSIZE	8
-
-class instruction1 {
-public:
-	unsigned char bytes[16];
-	int size;
-	string opr, opnd;
-};
-
-static csh cshandle = 0;
-static map<long long, instruction1> instructions;
 
 void errquit(const char *msg) {
 	perror(msg);
 	exit(-1);
 }
 
-int print_instruction(long long addr, instruction1 *in, const char *module) {
-	int i;
-	char bytes[128] = "";
-	if(in == NULL) {
-		fprintf(stderr, "0x%012llx<%s>:\t<cannot disassemble>\n", addr, module);
-	} else {
-		for(i = 0; i < in->size; i++) {  // print instruction base on instruction size
-			snprintf(&bytes[i*3], 4, "%2.2x ", in->bytes[i]);
-		}
-		// fprintf(stderr, "0x%012llx<%s>: %-32s\t%-10s%s\n", addr, module, bytes, in->opr.c_str(), in->opnd.c_str());
-		fprintf(stderr, "\t%06llx: %-32s\t%-10s%s\n", addr, bytes, in->opr.c_str(), in->opnd.c_str());
+uint8_t* code;
+long code_size;
+map<unsigned long, unsigned char> break_point_archive;  // (address, 1 byte content)
 
-        // cout << "in->size: " << in->size << endl; -> bingo
-        // printf("byte length +++++++ : %zu\n", strlen(bytes)); -> in->size * 5
-	}
+void load_code(char *argv[]) {
+    // load code
+    FILE* code_file = fopen(argv[1], "rb");
+    if (!code_file) {
+        printf("Failed to open binary file.\n");
+        return;
+    }
+    // get code_file size
+    fseek(code_file, 0, SEEK_END);
+    code_size = ftell(code_file);
+    rewind(code_file);
 
-    return in->size;
+    code = (uint8_t*)malloc(code_size);
+    if (!code) {
+        printf("Memory allocation failed.\n");
+        fclose(code_file);
+        return;
+    }
+
+    // memcpy();
+    size_t bytes_read = fread(code, 1, code_size, code_file);
+    if (bytes_read != static_cast<size_t>(code_size)) {
+        printf("Failed to read code from file.\n");
+        free(code);
+        fclose(code_file);
+        return;
+    }
+
+    fclose(code_file);
 }
 
-int disassemble(pid_t proc, unsigned long long rip, const char *module) {
-	int count;
-	char buf[64] = { 0 };
-	unsigned long long ptr = rip;
-	cs_insn *insn;
-	map<long long, instruction1>::iterator mi; // (memory address, instruction), from memory addr to instruction
-    int instruction_size;
 
-    // If ever recorded the instruction -> print & return
-	if((mi = instructions.find(rip)) != instructions.end()) {
-		instruction_size = print_instruction(rip, &mi->second, module);
-		return instruction_size;
-        // return print_instruction(rip, &mi->second, module);
-	}
+void load_next_five_instruction (unsigned long rip, unsigned long code_end_address) {
+    // using capstone
+    static csh cshandle = 0;
 
-	for(ptr = rip; ptr < rip + sizeof(buf); ptr += PEEKSIZE) {  // peek 64 byte at a time
-		long long peek;
-		errno = 0;  // directly use errno
-		peek = ptrace(PTRACE_PEEKTEXT, proc, ptr, NULL);
-		if(errno != 0) break;
-		memcpy(&buf[ptr-rip], &peek, PEEKSIZE);
-	}
+    if(cs_open(CS_ARCH_X86, CS_MODE_64, &cshandle) != CS_ERR_OK)
+			return;
 
-	if(ptr == rip)  {
-		instruction_size = print_instruction(rip, NULL, module);
-		return instruction_size;
-	}
+    size_t count;
+    cs_insn *insn;
+    // cout << "(rip - 0x400000): " << hex << (rip - 0x400000) << dec << endl;
+    count = cs_disasm(cshandle, code + (rip - 0x400000), code_size, rip, 5, &insn);  // code, sizeof code, start disasm addr, hm ins, disasm result memory pointer
+    // cout << "disasm count: " << count << endl;
 
-	if((count = cs_disasm(cshandle, (uint8_t*) buf, rip-ptr, rip, 0, &insn)) > 0) {
-		int i;
-		for(i = 0; i < count; i++) {
-			instruction1 in;
-			in.size = insn[i].size;
-			in.opr  = insn[i].mnemonic;
-			in.opnd = insn[i].op_str;
-			memcpy(in.bytes, insn[i].bytes, insn[i].size);
-			instructions[insn[i].address] = in;
-            // cout << "in.size: " << in.size << endl;
-            // cout << "in.opr: " << in.opr << endl;
-            // cout << "in.opnd: " << in.opnd << endl;
+    if (count > 0) {
+		for (size_t i = 0; i < count; i++) {
+            // disasm range check
+            if(insn[i].address > code_end_address) {
+                cout << "** the address is out of the range of the text section." << endl;
+                break;
+            }
+
+            unsigned char in_bytes[16];
+            memcpy(in_bytes, insn[i].bytes, insn[i].size);
+
+            // parse in_bytes to output formula "bytes"
+            // turn each byte to 2 hex value and append a " "
+            char bytes[128] = "";
+            for(int j = 0; j < insn[i].size; j++) {
+                // 2nd para is max byte to use for formatting output including '\0' -> 00 '\0' -> 4 bytes
+                snprintf(&bytes[j*3], 4, "%2.2x ", in_bytes[j]);
+            }
+
+            // cout << "insn[i].size: " << insn[i].size << endl;
+
+            // %06lx: output hex value with 6 char len (not enough padding 0 infront)
+            // %-32.*s: print insn[i].size amount of bytes
+            // %-10s: formatting output insn[i].mnemonic with 10 char len
+            // fprintf(stderr, "\t%06lx: %-32.*s\t%-10s%s\n", insn[i].address, insn[i].size * 3, bytes, insn[i].mnemonic, insn[i].op_str);
+    		fprintf(stderr, "\t%06lx: %-32s\t%-10s%s\n", insn[i].address, bytes, insn[i].mnemonic, insn[i].op_str);
 		}
+
 		cs_free(insn, count);
-	}
+	} else
+		printf("ERROR: Failed to disassemble given code!\n");
 
-	if((mi = instructions.find(rip)) != instructions.end()) {
-		instruction_size =print_instruction(rip, &mi->second, module);
-	} else {
-		instruction_size =print_instruction(rip, NULL, module);
-	}
-
-	return instruction_size;
+	cs_close(&cshandle);
+    return;
 }
 
 vector<string> split_string_by_space(const string& input) {
@@ -125,8 +122,7 @@ vector<string> split_string_by_space(const string& input) {
     return tokens;
 }
 
-long unsigned get_code_end_address(const string& command) {
-    // array<char, 128> buffer;
+void parse_elf(const string& command, unsigned long &code_start_address, unsigned long &code_end_address) {
     char buffer[1024];
     string result;
     FILE* pipe = popen(command.c_str(), "r");
@@ -136,23 +132,24 @@ long unsigned get_code_end_address(const string& command) {
     }
 
     string text_address;
-    int code_end_address = 0;
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
         // cout << "buffer: " << buffer << endl;
         if(string(buffer).find(".text") != string::npos) {
             text_appear = 1;
             // cout << "address: " << split_string_by_space(string(buffer))[4] << endl;
-            code_end_address = stoul(split_string_by_space(string(buffer))[4], nullptr, 16);
+            // code_end_address = stoul(split_string_by_space(string(buffer))[4], nullptr, 16);
+            code_start_address = stoul(split_string_by_space(string(buffer))[4], nullptr, 16);
 
         } else if (text_appear == 1) {
             text_appear = 2;
             // cout << "len: " << split_string_by_space(string(buffer))[0] << endl;
-            code_end_address += stoul(split_string_by_space(string(buffer))[0], nullptr, 16);
+            code_end_address = code_start_address + stoul(split_string_by_space(string(buffer))[0], nullptr, 16);
+            // code_end_address += stoul(split_string_by_space(string(buffer))[0], nullptr, 16);
         }
     }
     pclose(pipe);
-
-    return code_end_address - 1;  // start + len - 1 = end address
+    code_end_address -= 1;
+    return;
 }
 
 int main(int argc, char *argv[]) {
@@ -169,115 +166,101 @@ int main(int argc, char *argv[]) {
 	} else {
 		long long counter = 0LL;
 		int wait_status;
-		map<range_t, map_entry_t> m;
-		map<range_t, map_entry_t>::iterator mi;
-
-		if(cs_open(CS_ARCH_X86, CS_MODE_64, &cshandle) != CS_ERR_OK)
-			return -1;
-
 		if(waitpid(child, &wait_status, 0) < 0) errquit("waitpid");
 		ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
 
-		if(load_maps(child, m) > 0) {
-#if 0
-			for(mi = m.begin(); mi != m.end(); mi++) {
-				fprintf(stderr, "## %lx-%lx %04o %s\n",
-					mi->second.range.begin, mi->second.range.end,
-					mi->second.perm, mi->second.name.c_str());
-			}
-#endif
-			fprintf(stderr, "## %zu map entries loaded.\n", m.size());
-		}
+        // variable in while
+        string user_input;
+        struct user_regs_struct regs;
+        unsigned long code_start_address, code_end_address;
+        parse_elf("readelf -S " + string(argv[1]), code_start_address, code_end_address);
+        load_code(argv);
+		bool entry_point = true;
 
-        // see what in the m
-        // ## 400000-401000 0005 hello64
-        // ## 600000-601000 0007 hello64
-        // ## 7ffe40e14000-7ffe40e35000 0007 [stack]
-        // ## 7ffe40ebb000-7ffe40ebf000 0004 [vvar]
-        // ## 7ffe40ebf000-7ffe40ec1000 0005 [vdso]
-        // ## 7fffffffffffffff-7fffffffffffffff 0005 [vsyscall]
-        // cout << "========== m ============" << endl;
-        // for(mi = m.begin(); mi != m.end(); mi++) {
-        //     fprintf(stderr, "## %lx-%lx %04o %s\n",
-        //         mi->second.range.begin, mi->second.range.end,
-        //         mi->second.perm, mi->second.name.c_str());
-        // }
-        // cout << "========== m ============" << endl;
-        long unsigned code_end_address = get_code_end_address("readelf -S " + string(argv[1]));
-        cout << "code_end_address: " << hex << code_end_address << dec << endl;  // success
-
-        string user_input = "";
-        bool entry_point = true;
-
-		while (WIFSTOPPED(wait_status)) {
-			struct user_regs_struct regs;
+        while (WIFSTOPPED(wait_status)) {
 			counter++;
-			if(ptrace(PTRACE_GETREGS, child, 0, &regs) == 0) {
-                unsigned long instruction_address = regs.rip;
+            user_input = "";
 
-                if(entry_point) {
-                    // program './hello' loaded. entry point 0x401000
-                    // cout << "program '" + + "' loaded. entry point " +  << endl;
-                    fprintf(stdout, "program '%s' loaded. entry point 0x%lx\n", argv[1], instruction_address);
-                }
+            // [DONE] get register rip
+            if(ptrace(PTRACE_GETREGS, child, 0, &regs) == -1) perror("PTRACE_GETREGS");
 
-                // [DONE] - load five instructions ahead
-                for(int i = 0; i < 5; i++) {
-                    // fprintf(stderr, "0x%lx\n", instruction_address);
-                    range_t r = { instruction_address, instruction_address };  // use current rip to get 
-                    mi = m.find(r);  // find if the instruction ex: 0x4000b0 is in the map range
-                    if(mi == m.end()) {
-                        m.clear();
-                        fprintf(stderr, "## %zu map entries re-loaded.\n", m.size());
-                        mi = m.find(r);
-                    }
-                    instruction_address += disassemble(child, instruction_address, mi != m.end() ? mi->second.name.c_str() : "unknown");
+            if(entry_point) {
+                fprintf(stdout, "program '%s' loaded. entry point 0x%llx\n", argv[1], regs.rip);
+                entry_point = false;
+            }
 
-                    // [DONE] - break next time's disass since next time's asm exceed the text section
-                    if(instruction_address > code_end_address) {
-                        cout << "** the address is out of the range of the text section." << endl;
-                        break;
-                    }
-                }
-			}
+            // [DONE] load next five instruction based on rip
+            load_next_five_instruction(regs.rip, code_end_address);
 
-            // prompt
-            // wait for user input for next step
+            // [TODO] deal with user input and decide next step action
             while(1) {  // wait until valid input
                 cout << "(sdb) ";
                 user_input = "";
-                cin >> user_input;  // do not contain endline
+                getline(cin, user_input);
                 // cout << "user_input: " << user_input << endl;
                 // cout << "user_input len: " << user_input.length() << endl;
+                // cout << "user_input.substr(0, 6): " << user_input.substr(0, 6) << endl;
+
 
                 // [DONE] - support ins
                 //  1. si
                 //  2. cont
                 if (user_input == "si") {
                     if(ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) {
-                        perror("ptrace");
-                        cs_close(&cshandle);
+                        perror("PTRACE_SINGLESTEP");
+                        // cs_close(&cshandle);
                         return -2;
                     }
                     break;
+
                 } else if (user_input == "cont") {
                     if(ptrace(PTRACE_CONT, child, 0, 0) < 0) {
-                        perror("ptrace");
-                        cs_close(&cshandle);
+                        perror("PTRACE_CONT");
+                        // cs_close(&cshandle);
                         return -2;
                     }
                     break;
+                
+                } else if (user_input.substr(0, 6) == "break ") {
+                    unsigned long break_point_address = stoul(split_string_by_space(user_input)[1], nullptr, 16);
+
+                    // check the break point range
+                    if(break_point_address < code_start_address || break_point_address > code_end_address) {
+                        cout << " * break point address out of range." << endl;
+                        continue;
+
+                    }
+
+                    // set break point
+                    // -> overwrite the address's text to 0xcc and record the text
+                    // 1. record text -> 2. overwrite the address's text to 0xcc -> 3. for future recover
+                    // [remind] disasm cannot be 0xcc
+                    unsigned long peek_word = ptrace(PTRACE_PEEKTEXT, child, break_point_address, 0);
+                    // cout << "peek_word: " << hex << peek_word << dec << endl;
+                    break_point_archive[break_point_address] = static_cast<unsigned char>(peek_word & 0xff);
+                    // cout << "break_point_archive[break_point_address]: " << hex << static_cast<int>(break_point_archive[break_point_address]) << dec << endl;
+                    // cout << "(peek_word & 0xffffffffffffff00) | 0xcc): " << hex << ((peek_word & 0xffffffffffffff00) | 0xcc) << dec << endl;
+                    // ret = ptrace(PTRACE_POKETEXT, child, break_point_address, (restore_byte & 0xffffffffffffff00) | 0xcc);
+                    if(ptrace(PTRACE_POKETEXT, child, break_point_address, (peek_word & 0xffffffffffffff00) | 0xcc) != 0 ) errquit("POKETEXT");
+
+                    cout << "** set a breakpoint at 0x" << hex << break_point_address << dec << "." << endl;
+
+                    // peek_word = ptrace(PTRACE_PEEKTEXT, child, break_point_address, 0);
+                    // cout << "peek_word: " << hex << peek_word << dec << endl;
+
+                    // change it pack -> ok
+                    // if(ptrace(PTRACE_POKETEXT, child, break_point_address, (peek_word & 0xffffffffffffff00) | break_point_archive[break_point_address]) != 0 ) errquit("POKETEXT");
+                    // peek_word = ptrace(PTRACE_PEEKTEXT, child, break_point_address, 0);
+                    // cout << "peek_word: " << hex << peek_word << dec << endl;
+
                 } else {
-                    cout << "Undefined command: \"" + user_input + "\"." << endl;
+                    cout << " * Undefined command: \"" + user_input + "\"." << endl;
                 }
             }
-
-			if(waitpid(child, &wait_status, 0) < 0) errquit("waitpid");
-		}
-
-		// fprintf(stderr, "## %lld instructions(s) monitored\n", counter);
-        cout << "** the target program terminated." << endl;
-		cs_close(&cshandle);
+            
+            if(waitpid(child, &wait_status, 0) < 0) errquit("waitpid");        
+        }
+		fprintf(stderr, "## %lld instruction(s) executed\n", counter);
 	}
 	return 0;
 }
